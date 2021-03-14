0193760a10fc115e055e97b0b0be3cc14ff0dfff
HawkeyeUW <virtuej@outlook.com>
2021-03-13 19:39:15 -0500

    relationship

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
